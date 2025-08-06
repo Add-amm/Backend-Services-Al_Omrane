dd7f23c7ede56431a7fd4f9f883cbc5cdf7f0119
Adam Sorouri <adamsorouri8901@gmail.com>
2025-08-06 02:47:22 +0100

    updated models

--- services/file-service/src/models/file.js
+++ services/file-service/src/models/file.js
@@ -2,6 +2,7 @@ import { DataTypes } from 'sequelize';
 import sequelize from '../config/database.js';
 
 const File = sequelize.define('File', {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
   fileName: { type: DataTypes.STRING, allowNull: false },
   mimeType: { type: DataTypes.STRING },
   size: { type: DataTypes.INTEGER },
--- services/supplier-service/src/models/supplier.js
+++ services/supplier-service/src/models/supplier.js
@@ -6,7 +6,7 @@ const Supplier = sequelize.define('Supplier', {
     id_fiscal: { type: DataTypes.STRING, allowNull: false, unique: true },
     nom: { type: DataTypes.STRING, allowNull: false },
     n_tel: { type: DataTypes.STRING, allowNull: false, unique: true },
-    id_contrat: { type: DataTypes.INTEGER, allowNull: false }
+    file_id: { type: DataTypes.INTEGER, allowNull: false }
   }, {
     tableName: 'suppliers',
     timestamps: false
